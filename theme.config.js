b5578a367689f9cbea525a8aaf944983c72e1157
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-11-19 18:46:31 -0500

    cleanup

--- theme.config.js
+++ theme.config.js
@@ -4,8 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Michael D'Angelo.
       </small>
       <style jsx>{`
         footer {
